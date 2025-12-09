f436187750fdd5121d153816fa8e7bffac7fd0a2
DevArchanjo <leandroarchanjo.comercial@gmail.com>
2025-12-09 11:13:24 -0300

    Consulta simples com query methods

--- src/main/java/com/leandroarchanjo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leandroarchanjo/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.leandroarchanjo.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,10 @@ import com.leandroarchanjo.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	//esse método vai retornar uma lista de Post
+	//o nome do método vai ser findBy -> buscar por/encontrar por
+	//nome do atributo que queremos buscar neste caso é o Title
+	//na frente usamos a palavra Containing recebendo uma string como argumento
+	//a palavra IgnoreCase faz com que ao buscar um titulo o banco ignore letras maiúsculas e minúsculas
+	List<Post> findByTitleContainingIgnoreCase(String text); //-> só essa declaração faz com que o springData monte a consulta para nós
 }

f578f69bd241afc9fb12ba1ac02fda26ad3b6c9a
DevArchanjo <leandroarchanjo.comercial@gmail.com>
2025-12-10 09:09:43 -0300

    Consulta simples com @Query

--- src/main/java/com/leandroarchanjo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leandroarchanjo/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.leandroarchanjo.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.leandroarchanjo.workshopmongo.domain.Post;
@@ -10,10 +11,8 @@ import com.leandroarchanjo.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	//esse método vai retornar uma lista de Post
-	//o nome do método vai ser findBy -> buscar por/encontrar por
-	//nome do atributo que queremos buscar neste caso é o Title
-	//na frente usamos a palavra Containing recebendo uma string como argumento
-	//a palavra IgnoreCase faz com que ao buscar um titulo o banco ignore letras maiúsculas e minúsculas
-	List<Post> findByTitleContainingIgnoreCase(String text); //-> só essa declaração faz com que o springData monte a consulta para nós
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
